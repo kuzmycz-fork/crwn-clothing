1ee7054b810e4411c68230c7f69b025c888c84a0
Mark Kuzmycz <kuzmycz@marks-mbp.lan>
2019-09-18 21:02:53 +1000

    Implemnted firebase utils, included the ability to authenticate our users.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,32 @@ const config = {
     appId: "1:324016107808:web:2b40a7e800a50e15ca2745"
   };
 
+  export const createUserProfileDocument = async ( userAuth, additionalData ) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+  }
 
   firebase.initializeApp(config);
 
